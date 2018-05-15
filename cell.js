2ca69684547bf77db7dcbd8792eba797e8effe7e
MegaminxCode <Oliegrace11@gmail.com>
2018-05-15 21:36:17 +0100

    minesweeper

---
+++ cell.js
@@ -0,0 +1,117 @@
+function Cell(i, j, w) {
+	this.i = i;
+	this.j = j;
+	this.x = i * w;
+	this.y = j * w;
+	this.w = w;
+	this.neighbourCount = 0;
+	
+	this.bee = false;
+	this.revealed = false;
+	this.flag = false;
+	this.wrong = false;
+	this.won = false;
+	this.gameOver = false;
+}
+
+Cell.prototype.show = function() {
+	if (totalBees == 0) {
+		gameWon();
+	}
+	if(!this.gameover) {
+		stroke(0);
+		//noFill();
+		frameRate(4);
+		fill(random(0, 255), random(0, 255), random(0, 255));
+		rect(this.x, this.y, this.w, this.w);
+	}
+	if (this.revealed) {
+		if (this.gameOver && this.bee) {
+			fill(200);
+			rect(this.x, this.y, this.w, this.w);
+			fill(150);
+			ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
+		}else if(this.bee) {
+			fill(150);
+			ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);	
+		} else if(this.flag) {
+			fill(0);
+			rect(this.x, this.y, this.w, this.w);
+		} else if (this.wrong) {
+			fill('red');
+			rect(this.x, this.y, this.w, this.w);
+		} 
+		else if (this.won) {
+			fill('green');
+			rect(this.x, this.y, this.w, this.w);
+		}else if (this.gameOver) {
+			fill('orange');
+			rect(this.x, this.y, this.w, this.w);
+		}else {
+			fill(200);
+			rect(this.x, this.y, this.w, this.w);
+			if(this.neighbourCount > 0) {
+				textAlign(CENTER);
+				fill(0);
+				text(this.neighbourCount, this.x + this.w * 0.5, this.y + this.w - 6);
+			}
+		}
+		
+	}
+}
+
+Cell.prototype.countNeighbours = function (x, y) {
+	if (this.bee) {
+		this.neighbourCount = -1;
+		return;
+	}
+	var total = 0;
+	
+	for(xoff = -1; xoff <= 1; xoff++) {
+		for(yoff = -1; yoff <= 1; yoff++) {
+			var i = this.i + xoff;
+			var j = this.j + yoff;
+			if (i > -1 && i < cols && j > -1 && j < rows) {
+				var neighbour = grid[i][j];
+				if(neighbour.bee || neighbour.flag && !neighbour.wrong) {
+					total++;
+				}
+			}
+		}
+	}
+	this.neighbourCount = total;
+}
+
+Cell.prototype.contains = function (x, y) {
+	
+	return(x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w); 
+		
+	
+}
+
+Cell.prototype.reveal = function (x, y) {
+	this.revealed = true;		
+	
+	if (this.neighbourCount == 0) {
+		this.floodFill();
+	}
+}
+
+Cell.prototype.floodFill = function() {
+  for (var xoff = -1; xoff <= 1; xoff++) {
+    var i = this.i + xoff;
+    if (i < 0 || i >= cols) continue;
+
+    for (var yoff = -1; yoff <= 1; yoff++) {
+      var j = this.j + yoff;
+      if (j < 0 || j >= rows) continue;
+
+      var neighbor = grid[i][j];
+      // Note the neighbor.bee check was not required.
+      // See issue #184
+      if (!neighbor.revealed) {
+        neighbor.reveal();
+      }
+    }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,148 @@
+
+function make2DArray (cols, rows) {
+ var arr = new Array(cols);
+ for (var i = 0; i < arr.length; i++) {
+	arr[i] = new Array(rows);
+
+	}	
+	return arr;
+}
+
+
+var grid;
+var cols;
+var rows;
+var w = 20;
+
+var totalBees = 70;
+var flags = 20;
+
+
+function setup(){
+	createCanvas(441, 441);
+	cols = floor (width / w);
+	rows = floor (height / w);
+	grid = make2DArray(cols, rows);
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			
+			grid[i][j] = new Cell(i, j, w);
+			
+		}
+	}
+	
+	var options = [];
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			options.push([i, j]);
+		}
+	}
+	
+	for (var n = 0; n < totalBees; n++) {
+		var index = floor(random(options.length));
+		var choice = options[index];
+		var i = choice[0];
+		var j = choice[1];
+		options.splice(index, 1);
+		grid[i][j].bee = true;
+	}
+	
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			
+			grid[i][j].countNeighbours();
+			
+		}
+	}
+}
+
+function gameOver () {
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			if(grid[i][j].flag && !grid[i][j].wrong){
+				grid[i][j].gameOver = true;
+				grid[i][j].bee = true;
+				
+			}
+			if(!grid[i][j].wrong){
+				grid[i][j].gameOver = true;
+			}
+			grid[i][j].flag = false;
+			grid[i][j].revealed = true;
+			
+		}
+	}
+}
+
+function gameWon () {
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			
+			grid[i][j].flag = false;
+			grid[i][j].bee = false;
+			grid[i][j].wrong = false;
+			grid[i][j].won = true;
+			grid[i][j].revealed = true;
+			
+		}
+	}
+	
+}
+
+window.addEventListener("contextmenu", function(e) { e.preventDefault(); })
+function mousePressed () {
+	
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			
+			if(grid[i][j].contains(mouseX, mouseY)) {
+				if (mouseButton === LEFT) {
+					grid[i][j].reveal();
+				
+					if (grid[i][j].bee) {
+						gameOver();
+					}
+				}
+				if (mouseButton === RIGHT) {
+					if(!grid[i][j].revealed){
+						if (grid[i][j].bee){
+							totalBees--;
+						}else {
+							grid[i][j].wrong = true;
+							totalBees++;
+						}
+						grid[i][j].bee = false;
+						grid[i][j].revealed = true;
+						grid[i][j].flag = true;
+						
+						
+					}else if(grid[i][j].flag && !grid[i][j].wrong){
+						grid[i][j].revealed = false;
+						grid[i][j].flag = false;
+						grid[i][j].bee = true;
+						totalBees++;
+					} else if(grid[i][j].flag && grid[i][j].wrong) {
+						grid[i][j].revealed = false;
+						grid[i][j].flag = false;
+						grid[i][j].wrong = false;
+						grid[i][j].countNeighbours();
+						totalBees--;
+					}
+				}
+			}
+		}
+	}
+	
+}
+
+function draw(){
+	background(255);
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			
+			grid[i][j].show();
+			
+		}
+	}
+	
+}
